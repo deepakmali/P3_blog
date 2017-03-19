af90036f2eb9c9a2ba9fa3f1bd13cd47532ab3cc
Deepak <deepak3823@gmail.com>
2017-03-19 16:20:26 +0530

    Fixed code standards according to pep8 for data_model.py

--- data_model.py
+++ data_model.py
@@ -9,25 +9,28 @@ from google.appengine.ext import db
 def make_salt():
     return ''.join(random.choice(letters) for x in range(5))
 
-def make_pw_hash(username, password, salt = None):
+
+def make_pw_hash(username, password, salt=None):
     if not salt:
         salt = make_salt()
-    return '%s|%s' %(salt, 
-                     hashlib.sha256(username + password + salt).hexdigest())
+    return '%s|%s' % (salt,
+                      hashlib.sha256(username + password + salt).hexdigest())
+
 
 def valid_password(username, password, password_hash):
     salt = password_hash.split('|')[0]
     return password_hash == make_pw_hash(username, password, salt)
 
+
 # To store user details.
 class User(db.Model):
-    username = db.StringProperty(required = True)
-    password_hash = db.StringProperty(required = True)
+    username = db.StringProperty(required=True)
+    password_hash = db.StringProperty(required=True)
     email = db.StringProperty()
-    created = db.DateTimeProperty(auto_now_add = True)
+    created = db.DateTimeProperty(auto_now_add=True)
 
     # class method to search a user by username
-    @classmethod 
+    @classmethod
     def get_by_name(cls, user):
         return cls.all().filter('username = ', user).get()
 
@@ -38,27 +41,28 @@ class User(db.Model):
 
     # method to hash password and return user object
     @classmethod
-    def register(cls, username, password, email = None):
-        pw_hash = make_pw_hash(username=username ,password=password)
-        return cls(username = username,
-                   password_hash = pw_hash,
-                   email = email).put()
+    def register(cls, username, password, email=None):
+        pw_hash = make_pw_hash(username=username, password=password)
+        return cls(username=username,
+                   password_hash=pw_hash,
+                   email=email).put()
 
     # Method to return user object if login credentials are valid.
     @classmethod
     def login(cls, username, password):
-        user = cls.get_by_name(user = username)
+        user = cls.get_by_name(user=username)
         if user and valid_password(username, password, user.password_hash):
             return user
 
+
 # To store blog posts
 class BlogPosts(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
     created_by = db.ReferenceProperty(User)
-    total_likes = db.IntegerProperty(default = 0)
-    total_comments = db.IntegerProperty(default = 0)
+    total_likes = db.IntegerProperty(default=0)
+    total_comments = db.IntegerProperty(default=0)
 
     # method to return most recent 10 posts
     @classmethod
@@ -87,6 +91,7 @@ class BlogPosts(db.Model):
         self.total_comments -= 1
         self.put()
 
+
 # To store comments on posts
 class Comments(db.Model):
     post = db.ReferenceProperty(BlogPosts)
@@ -95,15 +100,15 @@ class Comments(db.Model):
 
     @classmethod
     def get_post_comments(cls, post):
-        if post :
+        if post:
             return cls.all().filter('post =', post)
 
     @classmethod
     def put_post_comments(cls, post, user, comment_text):
         if post and user and comment_text:
-            obj = Comments(post = post,
-                           user = user,
-                           comment_text = comment_text)
+            obj = Comments(post=post,
+                           user=user,
+                           comment_text=comment_text)
             obj.put()
             post.increment_comment_count()
 
@@ -115,7 +120,8 @@ class Comments(db.Model):
     def get_postId(cls, commentId):
         comment = cls.get_comment(commentId)
         return comment.post.key().id()
-        
+
+
 # Store the likes on posts
 class Likes(db.Model):
     post = db.ReferenceProperty(BlogPosts)
@@ -123,8 +129,8 @@ class Likes(db.Model):
 
     @classmethod
     def put_liked_user(cls, post, user):
-        if post and user :
-            cls(post = post, user = user).put()
+        if post and user:
+            cls(post=post, user=user).put()
             # obj.put()
             post.increment_like_count()
             # cls(post = post,
